be7aa5431e6a92787a9b38440ae1767a0cf8eee6
Alana Harmon <alana.harmon@yahoo.com>
2015-11-19 19:31:51 -0600

    Structure
    Added the index, css and js pages

---
+++ booksense.js
@@ -0,0 +1,9 @@
+function myFunction() {
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	var width = canvas.width;
+	var height = canvas.height;
+	
+	ctx.fillStyle = "rgb(0,162,232)";
+	ctx.fillRect(100,0,150,75);
+}// JavaScript Document
