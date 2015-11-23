c7164be9db85329982a9ab517e68c458d43fdec8
Alana Harmon <alana.harmon@yahoo.com>
2015-11-22 21:00:55 -0600

    main page and details
    On 11/22/15 added the main.html page , as the first page the user comes
    to when the app comes out of the splash screen. and details to other
    things.

--- booksense.js
+++ booksense.js
@@ -1,9 +1,28 @@
-function myFunction() {
-	var c = document.getElementById("myCanvas");
+
+// JavaScript Document
+function splashDot1() {
+	var c = document.getElementById("myCanvas2");
 	var ctx = c.getContext("2d");
 	var width = canvas.width;
 	var height = canvas.height;
 	
-	ctx.fillStyle = "rgb(0,162,232)";
-	ctx.fillRect(100,0,150,75);
-}// JavaScript Document
+	ctx.fillStyle = "rgb(0,162,232)"; // light blue
+	ctx.fillRect(50,50,35,30);
+	ctx.fillRect(95,40,45,40);
+	ctx.fillRect(150,30,35,50);
+	ctx.fillRect(205,60,35,5);
+	ctx.fillRect(205,50,35,8);
+	ctx.fillRect(205,46,35,12);
+	ctx.fillRect(202,39,40,6);
+	ctx.fillRect(202,26,40,12);
+	
+	ctx.beginPath();
+	ctx.fillStyle = "rgb(255,255,20)"; //yellow
+	ctx.arc(220,13,12,Math.PI*2,0,true);
+	ctx.fill();
+
+	ctx.beginPath();
+	ctx.strokeStyle = "rgb(0,162,232)"; //light blue
+	ctx.arc(222,68,17,Math.PI*1,Math.PI*2,true);
+	ctx.stroke();
+}
